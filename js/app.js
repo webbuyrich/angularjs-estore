e94f2d5fc82738fd36562f812ae0b66e053a2bca
Richard Peterson <webbuyrich@gmail.com>
2016-02-23 16:22:44 -0600

    added dummy json data

--- js/app.js
+++ js/app.js
@@ -8,20 +8,99 @@
 
 	var gems = [
 
-		{
-			name: 'Dodecahedron',
-			price: 2.95,
-			description: 'X number sided square or circle',
-			canPurchase: true,
-			soldOut: false
-		},
-		{
-			name: 'Foil Head',
-			price: 18.95,
-			description: 'Two tight scircle 3 circle',
-			canPurchase: true,
-			soldOut: false
-		}
+		
+		  {
+		    "name": "Otherway",
+		    "price": "$84.50",
+		    "description": "esse ex elit exercitation aliqua ipsum",
+		    "canPurchase": false,
+		    "soldOut": false
+		  },
+		  {
+		    "name": "Medicroix",
+		    "price": "$39.28",
+		    "description": "voluptate ad ex fugiat sint dolor",
+		    "canPurchase": false,
+		    "soldOut": false
+		  },
+		  {
+		    "name": "Tropolis",
+		    "price": "$79.38",
+		    "description": "laborum fugiat consectetur deserunt ipsum Lorem",
+		    "canPurchase": false,
+		    "soldOut": false
+		  },
+		  {
+		    "name": "Polaria",
+		    "price": "$37.03",
+		    "description": "eiusmod culpa duis tempor velit ipsum",
+		    "canPurchase": false,
+		    "soldOut": false
+		  },
+		  {
+		    "name": "Keengen",
+		    "price": "$46.98",
+		    "description": "veniam pariatur veniam minim ad deserunt",
+		    "canPurchase": true,
+		    "soldOut": false
+		  },
+		  {
+		    "name": "Rubadub",
+		    "price": "$20.54",
+		    "description": "ad aute pariatur eiusmod amet mollit",
+		    "canPurchase": false,
+		    "soldOut": false
+		  },
+		  {
+		    "name": "Kyagoro",
+		    "price": "$34.55",
+		    "description": "proident irure sit proident commodo quis",
+		    "canPurchase": false,
+		    "soldOut": false
+		  },
+		  {
+		    "name": "Otherside",
+		    "price": "$60.22",
+		    "description": "ad mollit adipisicing officia eiusmod deserunt",
+		    "canPurchase": false,
+		    "soldOut": false
+		  },
+		  {
+		    "name": "Slofast",
+		    "price": "$7.02",
+		    "description": "amet exercitation nisi aliquip pariatur proident",
+		    "canPurchase": false,
+		    "soldOut": false
+		  },
+		  {
+		    "name": "Parcoe",
+		    "price": "$82.38",
+		    "description": "ad occaecat excepteur minim culpa tempor",
+		    "canPurchase": false,
+		    "soldOut": false
+		  },
+		  {
+		    "name": "Quotezart",
+		    "price": "$53.06",
+		    "description": "ex eiusmod laboris exercitation laboris enim",
+		    "canPurchase": false,
+		    "soldOut": false
+		  },
+		  {
+		    "name": "Quizmo",
+		    "price": "$15.73",
+		    "description": "veniam sint enim pariatur mollit Lorem",
+		    "canPurchase": true,
+		    "soldOut": false
+		  },
+		  {
+		    "name": "Hivedom",
+		    "price": "$92.81",
+		    "description": "Lorem proident pariatur cillum dolore ad",
+		    "canPurchase": true,
+		    "soldOut": false
+		  }
+		
 
 
 	];
