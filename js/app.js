034266a1ee44b786bf00703be6aedecbe1e4e1e8
Richard Peterson <webbuyrich@gmail.com>
2016-02-23 12:17:32 -0600

    added ng-repeat to loop products

--- js/app.js
+++ js/app.js
@@ -3,15 +3,29 @@
 	var app = angular.module('store', []);
 
 	app.controller('StoreController', function(){
-		this.product = gem;
+		this.products = gems;
 	});
 
-	var gem = {
-		name: 'Dodecahedron',
-		price: 2.95,
-		description: 'X number sided square or circle',
-		canPurchase: true
-	}
+	var gems = [
+
+		{
+			name: 'Dodecahedron',
+			price: 2.95,
+			description: 'X number sided square or circle',
+			canPurchase: true,
+			soldOut: false
+		},
+		{
+			name: 'Foil Head',
+			price: 18.95,
+			description: 'Two tight scircle 3 circle',
+			canPurchase: true,
+			soldOut: false
+		}
+
+
+	];
+	
 
 
 
